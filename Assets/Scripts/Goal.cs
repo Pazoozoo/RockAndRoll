29d0d58aba363cacbe3f6b97c98ffa65c1ed9ab3
Ruben Rådelius <ruben.radelius@gmail.com>
2021-02-10 11:32:27 +0100

    adds goal

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.name == "Ball") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
