5ff4ebc0af317a67013911b6b3765e1c8ac346bb
Ruben Rådelius <ruben.radelius@gmail.com>
2021-02-10 14:44:45 +0100

    adds PointController scene and script

---
+++ Assets/Scripts/PointerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PointerController : MonoBehaviour
+{
+    public float speed = 1f;
+    public Rigidbody rb;
+
+    void Update() {
+        if (Input.GetMouseButton(0)) {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out var hitInfo);
+
+            var direction = hitInfo.point - transform.position;
+            rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
