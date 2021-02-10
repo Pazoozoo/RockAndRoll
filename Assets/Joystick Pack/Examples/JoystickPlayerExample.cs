41f0eeb4460aedc7ee1dcbbe48020f021f50836b
Ruben Rådelius <ruben.radelius@gmail.com>
2021-02-10 09:36:23 +0100

    adds Joystick

--- Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class JoystickPlayerExample : MonoBehaviour
 {
     public float speed;
-    public VariableJoystick variableJoystick;
+    public Joystick joystick;
     public Rigidbody rb;
 
     public void FixedUpdate()
     {
-        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
         rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
     }
 }
