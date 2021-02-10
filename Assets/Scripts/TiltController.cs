bd50247fadcc5c6e55b2aa4807d3582fd1fdacd0
Ruben Rådelius <ruben.radelius@gmail.com>
2021-02-10 17:38:23 +0100

    adds tilt controller and scene

---
+++ Assets/Scripts/TiltController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TiltController : MonoBehaviour
+{
+    public float speed = 1f;
+    public Rigidbody rb;
+
+    void Update() {
+        var direction = Input.acceleration * speed;
+        direction = Quaternion.Euler(90, 0, 0) * direction;
+        
+        rb.AddForce(direction * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+}
